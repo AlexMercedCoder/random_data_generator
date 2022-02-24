ea231170cdbc7babaa7770f5236a5a0f4c9443a3
Alex Merced <alex@alexmerced.dev>
2022-02-23 19:13:39 -0500

    update

--- generators/sales.js
+++ generators/sales.js
@@ -1,43 +1,44 @@
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const { faker } = require('@faker-js/faker');
-const format = require('date-fns/format')
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const { faker } = require("@faker-js/faker");
+const format = require("date-fns/format");
 
 // Create Writer
 const csvWriter = createCsvWriter({
-    path: '/home/alexmerced/development/scratch/data-generator/out/sales.csv',
-    header: [
-        {id: 'id', title: "sale_id"},
-        {id: 'purchaseDate', title: 'order_ts'},
-        {id: 'custId', title: 'customer_id'},
-        {id: 'productId', title: 'product_id'},
-        {id: 'productQty', title: 'product_qty'}
-    ]
+  path: "/home/alexmerced/development/scratch/data-generator/out/sales.csv",
+  header: [
+    { id: "id", title: "sale_id" },
+    { id: "purchaseDate", title: "order_ts" },
+    { id: "custId", title: "customer_id" },
+    { id: "productId", title: "product_id" },
+    { id: "productQty", title: "product_qty" },
+  ],
 });
 
 // Generate Records
-const records = []
+const records = [];
 
 // function to generate a random customer id
-const randCust = () => Math.floor(Math.random() * 10000) + 1
+const randCust = () => Math.floor(Math.random() * 10000) + 1;
 
 // function to generate a random product id
-const randProd = () => Math.floor(Math.random() * 5000) + 1
+const randProd = () => Math.floor(Math.random() * 5000) + 1;
 
-for(let i = 1; i < 100001; i++){
-    records.push({
-        id: i,
-        purchaseDate: format(faker.date.past(1), "yyyy-MM-dd HH:MM:SS"),
-        custId: randCust(),
-        productId: randProd(),
-        productQty: Math.floor(Math.random() * 9) + 1
-    })
+for (let i = 1; i < 100001; i++) {
+  records.push({
+    id: i,
+    purchaseDate: `${format(faker.date.past(1), "yyyy-MM-dd HH:MM:SS")}`,
+    custId: randCust(),
+    productId: randProd(),
+    productQty: Math.floor(Math.random() * 9) + 1,
+  });
 }
- 
+
 // Write CSV
-csvWriter.writeRecords(records)       // returns a promise
-    .then(() => {
-        console.log('...Done');
-    });
+csvWriter
+  .writeRecords(records) // returns a promise
+  .then(() => {
+    console.log("...Done");
+  });
 
 // This will produce a file path/to/file.csv with following contents:
 //
